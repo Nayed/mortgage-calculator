ec398e414eb7d394b42c8e08678f2aec8dc5b6d6
Nayed Saïd Ali <nayed.saidali@gmail.com>
2015-12-16 16:57:43 +0100

    amortization stuff

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -9,14 +9,47 @@ var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years,
     return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
 };
 
+var calculateAmortization = function calculateAmortization(principal, years, rate) {
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate);
+
+    var monthlyRate = _calculateMonthlyPaym.monthlyRate;
+    var monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
+
+    var balance = principal;
+    var amortization = [];
+
+    for (var y = 0; y < years; y++) {
+        var interestY = 0; // interest payment for year y
+        var principalY = 0; // principal payment for year y
+
+        for (var m = 0; m < 12; m++) {
+            var interestM = balance * monthlyRate; // interest payment for month m
+            var principalM = monthlyPayment - interestM; // principal payment for month m
+
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({ principalY: principalY, interestY: interestY, balance: balance });
+    }
+    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
+};
+
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var mortgage = calculateMonthlyPayment(principal, years, rate);
-    var monthlyPayment = mortgage.monthlyPayment;
-    var monthlyRate = mortgage.monthlyRate;
+
+    var _calculateAmortizatio = calculateAmortization(principal, years, rate);
+
+    var monthlyPayment = _calculateAmortizatio.monthlyPayment;
+    var monthlyRate = _calculateAmortizatio.monthlyRate;
+    var amortization = _calculateAmortizatio.amortization;
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+
+    amortization.forEach(function (month) {
+        return console.log(month);
+    });
 });
