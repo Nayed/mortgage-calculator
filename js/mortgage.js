d0a2e7cef5af6b4d63882de7e8742b5a4ab82dd2
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-17 01:08:41 +0100

    classy class

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,30 +1,37 @@
-export let calculateMonthlyPayment = (principal, years, rate) => {
-    let monthlyRate = 0
-    if (rate) {
-        monthlyRate = rate / 100 / 12
+class Mortgage {
+
+    constructor(principal, years, rate) {
+        this.principal = principal
+        this.years = years
+        this.rate = rate
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)))
-    return {principal, years, rate, monthlyPayment, monthlyRate}
-}
 
-export let calculateAmortization = (principal, years, rate) => {
-    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate)
-    let balance = principal
-    let amortization = []
+    get monthlyPayment() {
+        let monthlyRate = this.rate / 100 / 12
+        return this.principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), this.years * 12)))
+    }
 
-    for (let y = 0; y < years; y++) {
-        let interestY = 0       // interest payment for year y
-        let principalY = 0      // principal payment for year y
+    get amortization() {
+        let monthlyPayment = this.monthlyPayment
+        let monthlyRate = this.rate / 100 / 12
+        let balance = this.principal
+        let amortization = []
 
-        for (let m = 0; m < 12; m++) {
-            let interestM = balance * monthlyRate           // interest payment for month m
-            let principalM = monthlyPayment - interestM     // principal payment for month m
+        for (let y = 0; y < this.years; y++) {
+            let interestY = 0
+            let principalY = 0
 
-            interestY = interestY + interestM
-            principalY = principalY + principalM
-            balance = balance - principalM
+            for (let m = 0; m < 12; m++) {
+                let interestM = balance * monthlyRate
+                let principalM = monthlyPayment - interestM
+                interestY = interestY + interestM
+                principalY = principalY + principalM
+                balance = balance - principalM
+            }
+            amortization.push({principalY, interestY, balance})
         }
-        amortization.push({principalY, interestY, balance})
+        return amortization
     }
-    return {monthlyPayment, monthlyRate, amortization}
 }
+
+export default Mortgage
